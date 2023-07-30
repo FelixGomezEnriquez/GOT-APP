813b09ca55e4459134c86a144541315941c66f67
Felix <felixgomezenriquez@gmail.com>
2023-07-30 17:34:58 +0200

    añadido estilos login, register y forgot pass

--- src/app/modules/auth/forgot-password/forgot-password.component.ts
+++ src/app/modules/auth/forgot-password/forgot-password.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent {
-
+  constructor(
+    public auth: AuthService,
+    public router: Router,
+    private _snackBar: MatSnackBar
+  ) {}
+  sendEmail(email: string): void {
+    console.log(email);
+    this.auth.ForgotPassword(email);
+  }
+  openSnackBar(message: string, action: string, style: string[]): void {
+    this._snackBar.open(message, action, {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: style,
+    });
+  }
 }
