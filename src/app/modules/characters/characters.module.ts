af9add91ada594078763a5464b0481548a227930
Felix <felixgomezenriquez@gmail.com>
2023-09-11 12:42:00 +0200

    Estilando pagina HOME y pagina CHARACTERS

--- src/app/modules/characters/characters.module.ts
+++ src/app/modules/characters/characters.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 import { CharactersRoutingModule } from './characters-routing.module';
 import { CharacterComponent } from './character/character.component';
@@ -18,6 +20,8 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
     SharedModule,
     MatInputModule,
     FormsModule,
+    MatExpansionModule,
+    MatIconModule,
   ],
 })
 export class CharactersModule {}
