dcee47df1d902d451b8686e5c8f17cecaef86cab
Felix <felixgomezenriquez@gmail.com>
2023-09-11 14:04:37 +0200

    Implementacion de pagina no encontrada.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CoreRoutingModule } from './core-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AuthService } from '../services/auth.service';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-
+import { LayoutModule } from '../layout/layout.module';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [NotFoundComponent],
-  imports: [CommonModule, CoreRoutingModule, AngularFireAuthModule],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+    AngularFireAuthModule,
+    LayoutModule,
+    MatIconModule,
+  ],
   providers: [AuthService],
 })
 export class CoreModule {}
